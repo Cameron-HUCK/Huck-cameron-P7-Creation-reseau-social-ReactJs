6dbb3fe19fe3c5323979411e030dfb2e8de400de
DOZCAM <huck-cameron@hotmail.fr>
2022-06-21 20:50:32 +0200

    React

---
+++ frontend/client/src/App.js
@@ -0,0 +1,11 @@
+import React from "react-router-dom";
+import Home from '../src/pages/Home/index.'
+//import Auth from '../src/pages/Auth';
+//import Post from '../src/pages/Post';
+
+const App = () => {
+  return (
+      <Home />
+  )
+}
+export default App;
--- frontend/client/src/index.jsx
+++ frontend/client/src/index.jsx
@@ -1,24 +1,24 @@
 import React from 'react'
-import ReactDOM from 'react-dom'
+import ReactDOM from 'react-dom/client'
 import '../src/utils/styles/index.scss'
-import { BrowserRouter as Router, Route} from 'react-router-dom'
+import { BrowserRouter, Routes, Route} from 'react-router-dom'
 import Home from './pages/Home/index.';
-import Auth from './pages/Auth';
+import Auth from './pages/Auth/index';
 import Post from './pages/Post/index.';
 
-ReactDOM.render(
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
   <React.StrictMode>
-    <Router>
-            <Route exact path="/">
-              <Home />
-            </Route>
-            <Route path="/auth">
-              <Auth />
-            </Route>
-            <Route path="/post">
-              <Post />
-            </Route>
-    </Router>
-  </React.StrictMode>,
-  document.getElementById('root')
+    <BrowserRouter>
+      <Routes>
+        <Route exact path="/" element={<Home />} >
+        </Route>
+        <Route path="/auth" element={<Auth />} >
+          </Route>
+        <Route path="/post" element={<Post />}>
+          </Route>
+      </Routes>
+    </BrowserRouter>
+    </React.StrictMode>
 )
+
